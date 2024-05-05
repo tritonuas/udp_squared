dd07368af273d0401073947a8742c8ab0b2ff9d0
Tyler Lentz <tlentz@ucsd.edu>
2024-05-04 18:26:58 -0700

    add port info

--- protocol.h
+++ protocol.h
@@ -5,4 +5,7 @@
 #include "internal/packet.h"
 #include "internal/enum.h"
 
+#define UDP2_PAYLOAD_PORT 40907  // port the payloads listen on
+#define UDP2_OBC_PORT     40908  // port the obc listens on
+
 #endif  // PROTOCOL_H_
